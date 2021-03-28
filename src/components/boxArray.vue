1b97f6de01fbef944dcb5a1db05c080fb4a3de97
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-03-28 11:08:56 -0400

    https

--- src/components/boxArray.vue
+++ src/components/boxArray.vue
@@ -40,7 +40,7 @@ export default {
         }
     },
     created(){
-        fetch('http://winhacks2021-backend.builderpepc.repl.co/all_data',{cors:'no_cors'}).then(res=>res.json()).then(data=>{
+        fetch('https://winhacks2021-backend.builderpepc.repl.co/all_data',{cors:'no_cors'}).then(res=>res.json()).then(data=>{
            console.log(data.results);
             this.locations = data.results;
             
